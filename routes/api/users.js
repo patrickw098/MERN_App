ad2d99a94f40aa0383313834847a14e0f2815019
Patrick W <patricw2@uci.edu>
2018-09-17 13:23:08 -0700

    add authentication strategy

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
-const keys = require('../../config/keys')
+const keys = require('../../config/keys');
+const passport = require('passport');
+
+const User = require('../../models/User');
 
 router.get('/test', (req,res) => {
     res.json({ msg: "Users route is working"})
@@ -35,7 +37,7 @@ router.post('/register', (req,res) => {
 
                         jwt.sign(
                             payload,
-                            keys.secretOrkey,
+                            keys.secretOrKey,
                             { expiresIn: 3600 },
                             (err, token ) => {
                             res.json({
@@ -73,7 +75,7 @@ router.post("/login", (req, res) => {
                 const payload = { id: user.id, name: user.name };
 
                 jwt.sign(payload, 
-                    keys.secretOrKeys, 
+                    keys.secretOrKey, 
                     { expiresIn: 3600 }, 
                     (err, token) => {
                     res.json({
@@ -88,5 +90,12 @@ router.post("/login", (req, res) => {
         });
     });
 });
+
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.json({
+        id: req.user.id,
+        name: req.user.name
+    });
+})
 
 module.exports = router;
