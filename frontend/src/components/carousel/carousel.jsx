86bd4e807c618108d08bb2eb489b087ba76a10f9
Valerie Sui <valerie.k.sui@gmail.com>
2018-09-21 23:49:19 -0700

    add button functionality

--- frontend/src/components/carousel/carousel.jsx
+++ frontend/src/components/carousel/carousel.jsx
@@ -13,19 +13,53 @@ class Carousel extends React.Component {
         this.state = {
             images: imageDefaults,
             currentIdx: 0,
-            currentImages: [0,1,2]
+            currentImages: [0, 1, 2],
+            prevImages: [0, 1, 2]
         }
+
+        this.nextPhoto = this.nextPhoto.bind(this);
+        this.prevPhoto = this.prevPhoto.bind(this);
     }
 
     visitBusiness(){
         //go to business's page
     }
 
+    rotateImages(direction){
+        let { currentImages, images, prevImages, currentIdx } = this.state;
+        let newCurrent = currentImages.map((el) => el + direction);
+        const newIdx = currentIdx + direction;
+        
+        if (direction > 0 && newIdx < images.length - 3){
+            this.setState({
+                prevImages: currentImages
+            });
+        }
+
+    
+        newCurrent = newCurrent.reduce((acc, el) => {
+            if (el < images.length && el > -1) {
+                acc.push(el);
+            }
+            return acc;
+        }, []);
+    
+
+        if (direction < 0 && newIdx > images.length - 3){
+            newCurrent = prevImages;
+        }
+
+        return newCurrent;
+    }
+
     nextPhoto(e){
         e.preventDefault();
-        if(this.state.currentIdx < this.state.images.length - 1){
+        if(this.state.currentIdx < this.state.images.length - 3){
             this.setState({
-                currentIdx: this.state.currentIdx + 1
+                currentIdx: this.state.currentIdx + 3,
+                currentImages: this.rotateImages(1)
+            }, () => {
+                console.log(this.state);
             })
         }
 
@@ -36,24 +70,28 @@ class Carousel extends React.Component {
 
     prevPhoto(e){
         e.preventDefault();
-        if (this.state.currentIdx > 0) {
+        if (this.state.currentIdx > 2) {
             this.setState({
-                currentIdx: this.state.currentIdx - 1
+                currentIdx: this.state.currentIdx - 3,
+                currentImages: this.rotateImages(-1)
+            }, () => {
+                console.log(this.state);
             })
         }
     }
 
     render(){
-        const {images} = this.state;
+        const {images, currentImages} = this.state;
+        console.log('current',currentImages);
         return (
             <div className='carousel'>
-                <button className='button left-button'></button>
+                <button className='button left-button' onClick={this.prevPhoto}>Previous</button>
                 <div className="carousel-wrapper">
-                    {this.state.currentImages.map(index => {
+                    {currentImages.map(index => {
                         return <Photo key={images[index].id} url={images[index].url} />
                     })}
                 </div>
-                <button className='buttright-button'></button>
+                <button className='button right-button' onClick={this.nextPhoto}>More</button>
             </div>
         )
 
