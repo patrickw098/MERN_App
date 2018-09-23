ed8f2ea43a029c8caddeddd20a719663452f3c32
Patrick W <patricw2@uci.edu>
2018-09-23 14:31:53 -0700

    merge frontend

---
+++ frontend/src/config/vars.js
@@ -0,0 +1,8 @@
+export const imageDefaults = [
+    { url: "https://s3-media1.fl.yelpcdn.com/bphoto/NCX50ST3Hjg7y8wTcwQXog/o.jpg", id: 1 },
+    { url: "https://s3-media2.fl.yelpcdn.com/bphoto/cIH_FO2pXX0fXir4LGebxQ/o.jpg", id: 2 },
+    { url: "https://s3-media2.fl.yelpcdn.com/bphoto/lVSxE5BBIThDSXPVF8FFxw/o.jpg", id: 3 },
+    { url: "https://s3-media4.fl.yelpcdn.com/bphoto/L32x87s7OlEWatBGrkb3vA/o.jpg", id: 4 },
+    { url: "https://s3-media2.fl.yelpcdn.com/bphoto/b3ALUdMG7B0loFyDKYlH9w/o.jpg", id: 5 },
+    { url: "https://s3-media4.fl.yelpcdn.com/bphoto/_Qh2qyJChtmFfWCNIGhyjw/o.jpg", id: 6 }, 
+];
