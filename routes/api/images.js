e1baf3b7d554ec7d6d69f948fe50b8dc5ca76ec3
Patrick W <patricw2@uci.edu>
2018-09-23 16:20:12 -0700

    make basic api call

--- routes/api/images.js
+++ routes/api/images.js
@@ -2,14 +2,37 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
+const keys = require('../../config/keys');
+
 router.get('/test', (req, res) => {
     res.json({ msg: "Images route is working" })
 })
 
-router.get('/search', (req,res) => {
-    const query = req.data;
+router.post('/search', (req, res) => {
+    const { query } = req.body;
+    const options = {
+        headers: { 'Authorization': `Bearer ${keys.APIKey}`},
+        params: { term: query, location: keys.location },
+    }
 
+    axios.get('https://api.yelp.com/v3/businesses/search', options)
+        .then(payload => {
+            const id = payload.data.businesses[0].id
 
+            axios.get(`https://api.yelp.com/v3/businesses/${id}`, {
+                headers: { 'Authorization': `Bearer ${keys.APIKey}` }
+            })
+                .then( payload => {
+                    let photosArr = payload.data.photos;
+
+                    photosArr = photosArr.map( (photo,idx) => {
+                        return { id: idx, url: photo}
+                    })
+
+                    res.json(photosArr);
+                })
+        })
+        .catch(err => console.log(err))
 })
 
 module.exports = router;
